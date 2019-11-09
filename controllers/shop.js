c6184c9b720bf8ed7d22c014e7d3a0da89a7b2b9
alansarluv <alan.sarluf@gmail.com>
2019-11-09 16:58:09 +0700

    Display orders

--- controllers/shop.js
+++ controllers/shop.js
@@ -107,10 +107,15 @@ exports.postOrder = (req, res, next) => {
 };
 
 exports.getOrders = (req, res, next) => {
-  res.render('shop/orders', {
-    path: '/orders',
-    pageTitle: 'Your Orders'
-  })
+  Order.find({ 'user.userId': req.user._id })
+    .then(orders => {
+      res.render('shop/orders', {
+        path: '/orders',
+        pageTitle: 'Your Orders',
+        orders: orders
+      })
+    })
+    .catch(err => console.log(err));
 };
 
 exports.getCheckout = (req, res, next) => {
