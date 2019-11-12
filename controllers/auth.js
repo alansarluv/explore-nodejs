cadd0c6b4da59b7169d34310970dd6c59eddf9a9
alansarluv <alan.sarluf@gmail.com>
2019-11-13 01:16:17 +0700

    remove unuse param

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,8 +4,7 @@ const User = require('../models/user')
 exports.getLogin = (req, res, next) => {
   res.render('auth/login', {
     path: '/login',
-    pageTitle: 'Login',
-    isAuthenticated: req.session.isLoggedIn
+    pageTitle: 'Login'
   })
 };
 
@@ -48,8 +47,7 @@ exports.postLogout = (req, res, next) => {
 exports.getSignup = (req, res, next) => {
   res.render('auth/signup', {
     path: '/signup',
-    pageTitle: 'Signup',
-    isAuthenticated: false
+    pageTitle: 'Signup'
   });
 };
 
