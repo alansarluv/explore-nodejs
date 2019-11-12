88784f005b2d37888746d4111d10e797eb77170f
alansarluv <alan.sarluf@gmail.com>
2019-11-12 15:55:47 +0700

    basic update session & cookies

--- app.js
+++ app.js
@@ -4,32 +4,51 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
 const app = express();
+const store = new MongoDBStore({
+  uri: process.env.DB_DETAIL,
+  collection: 'sessions'
+})
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(
+  {
+    secret: 'my long string secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  }
+));
 
 app.use((req, res, next) => {
-  User.findById('5dc534f0961ffa1cf50e681c')
-    .then(user => {
+  if(!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+  .then(user => {
       req.user = user;
       next();
     })
-    .catch(err => console.log(err)); 
+    .catch(err => console.log(err));   
 })
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
