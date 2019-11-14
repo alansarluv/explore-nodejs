27243d5f9757b94cc5b6541b405b64d6239804e1
alansarluv <alan.sarluf@gmail.com>
2019-11-14 16:26:13 +0700

    email & password validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,8 @@ const bcrypt = require('bcryptjs')
 const nodemailer = require('nodemailer')
 const sendGridTransport =  require('nodemailer-sendgrid-transport')
 
+const { validationResult } = require('express-validator/check');
+
 const User = require('../models/user')
 const transporter = nodemailer.createTransport(sendGridTransport({
   auth: {
@@ -79,7 +81,15 @@ exports.getSignup = (req, res, next) => {
 exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
-  const confirmPassword = req.body.confirmPassword;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors.array());
+    return res.status(422).render('auth/signup', {
+      path: '/signup',
+      pageTitle: 'Signup',
+      errorMessage: errors.array()[0].msg
+    });
+  }
   User.findOne({email: email})
     .then(userDoc => {
       if (userDoc) {
--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,40 @@
 const express = require('express');
+const { check, body } = require('express-validator/check');
 const authController = require('../controllers/auth')
 const router = express.Router();
 
 router.get('/login', authController.getLogin)
 router.get('/signup', authController.getSignup)
 router.post('/login', authController.postLogin)
-router.post('/signup', authController.postSignup)
+router.post(
+  '/signup',
+  [
+    check('email')
+      .isEmail()
+      .withMessage('Please enter a valid email')
+      .custom((value, {req}) => {
+        if (value === 'dummy@dummy.dum') {
+          throw new Error('This dummy is only for test custom validation.')
+        } else {
+          return true;
+        }
+      }),
+    body(
+      'password',
+      'Please enter a password with only numbers and text at least 5 characters.'
+      )
+      .isLength({ min: 5 })
+      .isAlphanumeric(),
+    body('confirmPassword')
+      .custom((value, {req}) => {
+        if (value !== req.body.password) {
+          throw new Error('Password have to match!')
+        } else {
+          return true;
+        }
+      })
+  ]
+  , authController.postSignup);
 router.post('/logout', authController.postLogout)
 router.get('/reset', authController.getReset)
 router.post('/reset', authController.postReset)
